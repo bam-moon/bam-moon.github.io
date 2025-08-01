740d277eb2a71544ab2ec88a9f5fa03eab05bdcb
WZS <841473215@qq.com>
2025-08-01 09:32:44 +0800

    更新配置

--- src/config.ts
+++ src/config.ts
@@ -1,38 +1,40 @@
 export default {
   // 网站标题
-  Title: '韩小韩博客',
+  Title: "深竹的小憩地",
   // 网站地址
-  Site: 'https://www.vvhan.com',
+  Site: "https://bam-moon.github.io",
   // 网站副标题
-  Subtitle: '不曾与你分享的时间,我在进步.',
+  Subtitle: "去有风的地方.",
   // 网站描述
-  Description: '韩小韩博客 专注于前开发与相关技术的实战分享，涵盖Vue框架、Node.js、Serverless等，并涉及Node、Python、Linux、Docker等领域。同时，博客也分享作者的生活、音乐和旅行的热爱。',
+  Description:
+    "只想躺平的一个普通人，AI算法工程师",
   // 网站作者
-  Author: '.𝙃𝙖𝙣',
+  Author: "深竹",
   // 作者头像
-  Avatar: 'https://q1.qlogo.cn/g?b=qq&nk=1655466387&s=640',
+  Avatar: "https://wp-cdn.4ce.cn/v2/lx6YsQN.png",
   // 网站座右铭
-  Motto: '运气是计划之外的东西.',
+  Motto: "运气是计划之外的东西.",
   // Cover 网站缩略图
-  Cover: '/assets/images/banner/072c12ec85d2d3b5.webp',
+  Cover: "/assets/images/banner/072c12ec85d2d3b5.webp",
   // 网站侧边栏公告 (不填写即不开启)
-  Tips: '<p>欢迎光临我的博客 🎉</p><p>这里会分享我的日常和学习中的收集、整理及总结，希望能对你有所帮助:) 💖</p>',
+  Tips: "<p>欢迎光临我的博客 🎉</p><p>这里会分享我的日常和学习中的收集、整理及总结，希望能对你有所帮助:) 💖</p>",
   // 首页打字机文案列表
   TypeWriteList: [
-    '不曾与你分享的时间,我在进步.',
-    "I am making progress in the time I haven't shared with you.",
+    "如果花开的时候就想着终将花落，那开花就成了悲剧的一环.",
+    "If you think that the flowers will eventually fall when they bloom, then flowering will become a part of the tragedy.",
   ],
   // 网站创建时间
-  CreateTime: '2021-09-01',
+  CreateTime: "2025-08-01",
   // 顶部 Banner 配置
   HomeBanner: {
     enable: true,
     // 首页高度
-    HomeHeight: '38.88rem',
+    HomeHeight: "38.88rem",
     // 其他页面高度
-    PageHeight: '28.88rem',
+    PageHeight: "28.88rem",
     // 背景
-    background: "url('/assets/images/home-banner.webp') no-repeat center 60%/cover",
+    background:
+      "url('/assets/images/home-banner.webp') no-repeat center 30%/cover",
   },
   // 博客主题配置
   Theme: {
@@ -52,23 +54,28 @@ export default {
   Navs: [
     // 仅支持 SVG 且 SVG 需放在 public/assets/images/svg/ 目录下，填入文件名即可 <不需要文件后缀名>（封装了 SVG 组件 为了极致压缩 SVG）
     // 建议使用 https://tabler.io/icons 直接下载 SVG
-    { text: '朋友', link: '/links', icon: 'Nav_friends' },
-    { text: '圈子', link: '/friends', icon: 'Nav_rss' },
-    { text: '动态', link: '/talking', icon: 'Nav_talking' },
-    { text: '昔日', link: '/archives', icon: 'Nav_archives' },
-    { text: '留言', link: '/message', icon: 'Nav_message' },
-    { text: '关于', link: '/about', icon: 'Nav_about' },
-    { text: 'API', link: 'https://api.vvhan.com/', target: true, icon: 'Nav_link' },
+    { text: "文章", link: "/archives", icon: "Nav_archives" },
+    { text: "朋友", link: "/links", icon: "Nav_friends" },
+    { text: "圈子", link: "/friends", icon: "Nav_rss" },
+    { text: "动态", link: "/talking", icon: "Nav_talking" },
+    { text: "留言", link: "/message", icon: "Nav_message" },
+    { text: "本人", link: "/about", icon: "Nav_about" },
   ],
   // 侧边栏个人网站
   WebSites: [
     // 仅支持 SVG 且 SVG 需放在 public/assets/images/svg/ 目录下，填入文件名即可 <不需要文件后缀名>（封装了 SVG 组件 为了极致压缩 SVG）
     // 建议使用 https://tabler.io/icons 直接下载 SVG
-    { text: 'Github', link: 'https://github.com/uxiaohan', icon: 'WebSite_github' },
-    { text: '韩小韩API', link: 'https://api.vvhan.com', icon: 'WebSite_api' },
-    { text: '每日热榜', link: 'https://hot.vvhan.com', icon: 'WebSite_hot' },
-    { text: '骤雨重山图床', link: 'https://wp-cdn.4ce.cn', icon: 'WebSite_img' },
-    { text: 'HanAnalytics', link: 'https://analytics.vvhan.com', icon: 'WebSite_analytics' },
+    {
+      text: "Github",
+      link: "https://github.com/wqangzuoshuai",
+      icon: "WebSite_github",
+    },
+    { text: "每日热榜", link: "https://hot.vvhan.com", icon: "WebSite_hot" },
+    {
+      text: "骤雨重山图床",
+      link: "https://wp-cdn.4ce.cn",
+      icon: "WebSite_img",
+    },
   ],
   // 侧边栏展示
   AsideShow: {
@@ -79,55 +86,59 @@ export default {
     // 是否展示标签
     TagsShow: true,
     // 是否展示推荐文章
-    recommendArticleShow: true
+    recommendArticleShow: true,
   },
   // DNS预解析地址
   DNSOptimization: [
-    'https://i0.wp.com',
-    'https://cn.cravatar.com',
-    'https://analytics.vvhan.com',
-    'https://vh-api.4ce.cn',
-    'https://registry.npmmirror.com',
-    'https://pagead2.googlesyndication.com'
+    "https://i0.wp.com",
+    "https://cn.cravatar.com",
+    "https://analytics.vvhan.com",
+    "https://vh-api.4ce.cn",
+    "https://registry.npmmirror.com",
+    "https://pagead2.googlesyndication.com",
   ],
   // 博客音乐组件解析接口
-  vhMusicApi: 'https://vh-api.4ce.cn/blog/meting',
+  vhMusicApi: "https://vh-api.4ce.cn/blog/meting",
   // 评论组件（只允许同时开启一个）
   Comment: {
     // Twikoo 评论
     Twikoo: {
       enable: false,
-      envId: ''
+      envId: "",
     },
     // Waline 评论
     Waline: {
       enable: false,
-      serverURL: ''
-    }
+      serverURL: "",
+    },
   },
   // Han Analytics 统计（https://github.com/uxiaohan/HanAnalytics）
-  HanAnalytics: { enable: true, server: 'https://analytics.vvhan.com', siteId: 'Hello-HanHexoBlog' },
+  //HanAnalytics: {
+  //  enable: true,
+  //  server: "https://analytics.vvhan.com",
+  //  siteId: "Hello-HanHexoBlog",
+  //},
   // Google 广告
   GoogleAds: {
-    ad_Client: '', //ca-pub-xxxxxx
+    ad_Client: "", //ca-pub-xxxxxx
     // 侧边栏广告(不填不开启)
     asideAD_Slot: `<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-xxxxxx" data-ad-slot="xxxxxx" data-ad-format="auto" data-full-width-responsive="true"></ins>`,
     // 文章页广告(不填不开启)
-    articleAD_Slot: `<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-xxxxxx" data-ad-slot="xxxxxx" data-ad-format="auto" data-full-width-responsive="true"></ins>`
+    articleAD_Slot: `<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-xxxxxx" data-ad-slot="xxxxxx" data-ad-format="auto" data-full-width-responsive="true"></ins>`,
   },
   // 文章内赞赏码
   Reward: {
     // 支付宝收款码
-    AliPay: '/assets/images/alipay.webp',
+    AliPay: "",
     // 微信收款码
-    WeChat: '/assets/images/wechat.webp'
+    WeChat: "",
   },
   // 访问网页 自动推送到搜索引擎
   SeoPush: {
     enable: false,
-    serverApi: '',
-    paramsName: 'url'
+    serverApi: "",
+    paramsName: "url",
   },
   // 页面阻尼滚动速度
-  ScrollSpeed: 666
-}
+  ScrollSpeed: 300,
+};
